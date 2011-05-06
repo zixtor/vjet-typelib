ffc694ae84ffdbff2a140a6fc9287460e6ace1ee
earlyster <earlyster@27f4aac7-f869-4a38-a8c2-f1a995e726e6>
2011-05-06 16:50:02 +0000

    Adding PerformanceTimingTL based on the draft w3c spec https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/NavigationTiming/Overview.html
    git-svn-id: http://www.ebayopensource.org/svn/vjet/trunk/typelib@228 27f4aac7-f869-4a38-a8c2-f1a995e726e6

---
+++ PerformanceTimingTL/src/org/w3c/Performance.js
@@ -0,0 +1,14 @@
+vjo.ctype('org.w3c.Performance') //< public
+//<needs(org.w3c.PerformanceTiming)
+//<needs(org.w3c.PerformanceNavigation)
+.globals({
+	performance:null //< org.w3c.Performance
+})
+.protos({
+		timing:null, //< public org.w3c.PerformanceTiming
+		navigation:null //< public org.w3c.PerformanceNavigation
+})
+.options({
+	metatype:true
+})
+.endType();
---
+++ PerformanceTimingTL/src/org/w3c/WindowExt.js
@@ -0,0 +1,5 @@
+vjo.ctype('org.w3c.WindowExt') //< public
+.globals({
+	performance:null //< org.w3c.Performance
+}, window)
+.endType();
