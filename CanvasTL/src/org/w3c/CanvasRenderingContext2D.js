2f7878ac63d906495390ca009718b9c5d45ea29b
Justin Early <earlyster@gmail.com>
2012-08-28 15:45:44 -0700

    CanvasTL rename and copyright fixes

--- CanvasTL/src/org/w3c/CanvasRenderingContext2D.js
+++ CanvasTL/src/org/w3c/CanvasRenderingContext2D.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 eBay Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     eBay Inc. - initial API and implementation
+ *******************************************************************************/
 /**> public ;
  *The <dfn id="dom-context-2d-canvas" title="dom-context-2d-canvas"><code>canvas</code></dfn>
   attribute must return the <code>canvas</code> element that the
