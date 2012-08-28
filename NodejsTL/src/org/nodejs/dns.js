984ba135d98c8f94cb1e794d8c84359028eeb3af
Justin Early <earlyster@gmail.com>
2012-08-28 15:53:58 -0700

    fixing NodeJs TL adding copyright

--- NodejsTL/src/org/nodejs/dns.js
+++ NodejsTL/src/org/nodejs/dns.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 eBay Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     eBay Inc. - initial API and implementation
+ *******************************************************************************/
 /**
  * Use require('dns') to access this module.
  * <p>Here is an example which resolves 'www.google.com' then reverse resolves 
--- NodejsTL/src/org/nodejs/node.js
+++ NodejsTL/src/org/nodejs/node.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 eBay Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     eBay Inc. - initial API and implementation
+ *******************************************************************************/
 // top-level module is node.js.  We expose built-in global entities that 
 // node.js provides out-of-the-box.  
 /**
